5050bedf4bf80c450eef116ac9b50517bc6c6def
Vissu2016 <128463919+Vissu2016@users.noreply.github.com>
2023-03-23 14:39:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry OAt Meal')
 streamlit.text('Kale, Spinach & Rocket Sommthie')
